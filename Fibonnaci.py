bb95f499bc630cf8bd3a36a8283caea5fe8e200f
shifacodingal <shifacodingal@gmail.com>
2026-08-17 17:01:22 +0530

    Fibonnaci Series

---
+++ Fibonnaci.py
@@ -0,0 +1,15 @@
+# Python program to print Fibonacci series
+
+n = int(input("Enter the number of terms: "))
+
+a = 0
+b = 1
+
+print("Fibonacci Series:")
+
+for i in range(n):
+    print(a, end=" ")
+    
+    c = a + b
+    a = b
+    b = c
